89ae2caa875361110fae5ba06e3193f3eded11aa
Aris Pavlides <aris.pavlides@marinetraffic.com>
2018-09-14 18:56:21 +0300

    Exercise set 3 completed

--- Exercises/Third_Set/Answers/Answers/Program.cs
+++ Exercises/Third_Set/Answers/Answers/Program.cs
@@ -6,12 +6,20 @@ using System.Threading.Tasks;
 
 namespace Answers
 {
+    enum Q3
+    {
+        ArrayLength = 5
+    }
+
     class Program
     {
         static void Main()
         {
-            //Question_1();
-            Question_2();
+            // Question_1();
+            // Question_2();
+            // Question_3();
+            // Question_4();
+            Question_5();
         }
 
         static void Question_1()
@@ -53,6 +61,89 @@ namespace Answers
         {
             Console.Write("Enter your name: ");
             string userName = Console.ReadLine();
+
+            char[] letterArr = new char[userName.Length];
+
+            int arrPointer = userName.Length - 1;
+
+            foreach (char letter in userName)
+            {
+                letterArr[arrPointer] = letter;
+                arrPointer--;
+            }
+
+            Console.WriteLine(letterArr);
+            Console.ReadLine();
+
+        }
+
+        static void Question_3()
+        {
+            int countNums = 0;
+            int[] numsList = new int[(int)Q3.ArrayLength];
+
+            while (countNums < (int)Q3.ArrayLength)
+            {
+                Console.Write("Enter a number you have not entered before: ");
+                string numberStr = Console.ReadLine();
+                int number = Convert.ToInt32(numberStr);
+
+                if (!numsList.Contains(number))
+                {
+                    numsList[countNums] = number;
+                    countNums++;
+                }
+            }
+
+            Array.Sort(numsList);
+
+            foreach (int number in numsList) { Console.Write(number); }
+
+            Console.ReadLine();
+        }
+
+        static void Question_4()
+        {
+            List<int> userNumbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string numStr = Console.ReadLine();
+
+                if (numStr.ToUpper() == "OK"){ break; }
+
+                else
+                {
+                    int numInt = Convert.ToInt32(numStr);
+                    userNumbers.Add(numInt);
+                }
+            }
+
+            foreach (int num in userNumbers.Distinct()){ Console.WriteLine(num); }
+            Console.ReadLine();            
+        }
+
+        static void Question_5()
+        {
+            List<string> userListStr = new List<string>();
+            while (true)
+            {
+                Console.Write("Supply a list of comma separated numbers: ");
+                string userStr = Console.ReadLine();
+
+                userListStr.Clear();
+                userListStr = userStr.Split(',').ToList();
+
+                if (userListStr.Count >= 5) { break; }
+                else { Console.WriteLine("Enter a sequence of 5 numbers or more!!"); }
+            }
+
+            List<int> userList = userListStr.Select(int.Parse).ToList();
+            userList.Sort();
+
+            Console.WriteLine("The three smallest numbers are: {0}, {1} and {2} respectively", userList[0], userList[1], userList[2]);
+            Console.ReadLine();
         }
     }
 }
