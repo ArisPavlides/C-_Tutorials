05149ae1468537fc64b15ed94fb3ea8d50265210
Aris Pavlides <arispavlides86@gmail.com>
2018-09-15 15:21:49 +0300

    added DateTime & TimeSpan tutorials

---
+++ Date_Time/Date_Time/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime dateTime = new DateTime(2017, 9, 15); // create a new DateTime object with the 3 property values
+            DateTime now = DateTime.Now; // now is a static method within the DateTime class 
+            int minute = DateTime.Now.Minute;
+
+            // DateTime objects are immutable so the only way to change them after they are  being created is to either recreate
+            // them or use the .Add method
+            DateTime future = now.AddDays(1);
+            DateTime halfHourAgo = now.AddMinutes(-30);
+
+            // DateTime to string
+            string longDateString = now.ToLongDateString(); // 15 September 2018
+            string shortDateString = now.ToShortDateString(); // 15/09/2018
+            string longTimeString = now.ToLongTimeString(); // 12:28:33 
+            string shortTimeString = now.ToShortTimeString(); // 12:28 
+            string dateTimeStr = now.ToString(); // 15/09/2018 12:28:33 
+            string yearStr = now.ToString("MM/dd/yyyy");
+        }
+    }
+}
---
+++ Timespan/Timespan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timespan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan timespan = new TimeSpan(1, 2, 3); // input parameters are hours,minutes,seconds (i.e. 1 hour, 2 minutes & 3 seconds)
+            int spanMins =  timespan.Minutes; // returns the minutes part of the object above (i.e. 2)
+            double spanMinsTotal = timespan.TotalMinutes; // returns the total timespn above in minutes (i.e. 60+2+1/20)
+
+            var time = TimeSpan.FromDays(3.5); // returns 3 days and 12 hours
+
+            // deducting DateTimes returns TimeSpan (adding is not supported)
+            DateTime date_1 = DateTime.Now;
+            DateTime date_2 = DateTime.Now.AddDays(5);
+            TimeSpan dateSubt = date_2 - date_1;
+
+            // TimeSpan objects are immutable but can be changed used the add method by adding another timespan object
+            TimeSpan timeSpanAdd = timespan.Add(TimeSpan.FromHours(3));
+
+            // convert a TimeSpan object to string
+            string timeSpanStr = new TimeSpan(1, 3, 5).ToString();
+
+            // convert a string to TimeSpan
+            TimeSpan spanFromStr = TimeSpan.Parse("03:05:07");
+        }
+    }
+}
